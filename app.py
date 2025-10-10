baf28c24e9fa333a99470f24103c7829080e628f
Yimin Liu <yl10843@nyu.edu>
2025-10-09 21:16:30 -0400

    add python env and route

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return "✅ Flask is running successfully! Your project is set up."
 
+@app.route('/login')
+def login():
+    return render_template('todo_login.html')
+
+@app.route('/add-task')
+def add_task():
+    return render_template('todo_add_task.html')
+
+@app.route('/history')
+def history():
+    return render_template('todo_history.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
